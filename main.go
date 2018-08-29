f04bd96bee9c31ceca40e9e6998ebd82e0b86eb8
Justin Clift <justin@postgresql.org>
2018-08-29 10:11:40 +0100

    Simplify even further.  Remove mouse handler, and just show a simple rectangle onscreen

--- main.go
+++ main.go
@@ -3,18 +3,12 @@
 package main
 
 import (
-	"math"
 	"syscall/js"
-
-	"github.com/lucasb-eyer/go-colorful"
 )
 
 var (
 	width  float64
 	height float64
-	mouseX float64
-	mouseY float64
-	size   = 25
 )
 
 func main() {
@@ -29,22 +23,11 @@ func main() {
 
 	done := make(chan struct{}, 0)
 
-	mouseMoveEvt := js.NewCallback(func(args []js.Value) {
-		e := args[0]
-		mouseX = e.Get("clientX").Float()
-		mouseY = e.Get("clientY").Float()
-		//fmt.Printf("Mouse position - X: %v, Y: %v\n", mouseX, mouseY)
-	})
-	defer mouseMoveEvt.Release()
-
-	// Handle mouse
-	doc.Call("addEventListener", "mousemove", mouseMoveEvt)
-
 	var renderFrame js.Callback
-	colorRot := float64(0)
-	currentX := float64(100)
-	currentY := float64(75)
-
+	rectX := 20
+	rectY := 20
+	rectHeight := 30
+	rectWidth := 30
 	renderFrame = js.NewCallback(func(args []js.Value) {
 		// Handle window resizing
 		curBodyW := doc.Get("body").Get("clientWidth").Float()
@@ -55,19 +38,24 @@ func main() {
 			canvasEl.Set("height", height)
 		}
 
-		// Work out where to draw the next shape
-		moveX := (mouseX - currentX) * 0.02
-		moveY := (mouseY - currentY) * 0.02
-		currentX += moveX
-		currentY += moveY
+		// ** Draw the frame content **
 
-		// Draw the shape at the new position
-		colorRot = float64(int(colorRot+1) % 360)
-		ctx.Set("fillStyle", colorful.Hsv(colorRot, 1, 1).Hex())
-		ctx.Call("beginPath")
-		ctx.Call("arc", currentX, currentY, size, 0, 2*math.Pi) // Radians
-		ctx.Call("fill")
+		// Grey background
+		ctx.Set("fillStyle", "lightgrey")
+		ctx.Call("fillRect", 1, 1, width-1, height-1)
 
+		// TODO: Other stuff
+
+		// Draw a simple square
+		ctx.Set("fillStyle", "black")
+		ctx.Call("fillRect", rectX, rectY, rectWidth, 2)
+		ctx.Call("fillRect", rectX, rectY, 2, rectHeight)
+		ctx.Call("fillRect", rectX+rectWidth, rectY, 2, rectHeight+2)
+		ctx.Call("fillRect", rectX, rectY+rectHeight, rectWidth, 2)
+
+
+		// It seems kind of weird to recursively call itself here, instead of using a timer approach, but apparently
+		// this is best practise (at least in web environments: https://css-tricks.com/using-requestanimationframe)
 		js.Global().Call("requestAnimationFrame", renderFrame)
 	})
 	defer renderFrame.Release()
