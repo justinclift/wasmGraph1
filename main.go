19bb6fd9964fae42b99af38eb32e56f21d63c97d
Justin Clift <justin@postgresql.org>
2018-08-30 00:52:07 +0100

    Add some more objects, add useful matrix manipulation functions (non-optimised)

--- main.go
+++ main.go
@@ -33,12 +33,23 @@ var (
 		{X: -1.5, Y: -1.75, Z: 1.0},
 		{X: 0, Y: 0, Z: 1.75},
 	}
+	object2 = Object{
+		{X: 1.5, Y: 1.5, Z: -1.0},
+		{X: 1.5, Y: -1.5, Z: -1.0},
+		{X: -1.5, Y: -1.5, Z: -1.0},
+	}
+	object3 = Object{
+		{X: 2, Y: -2, Z: 1.0},
+		{X: 2, Y: -4, Z: 1.0},
+		{X: -2, Y: -4, Z: 1.0},
+		{X: -2, Y: -2, Z: 1.0},
+		{X: 0, Y: -3, Z: 2.5},
+	}
 
-	width  float64
-	height float64
-	//debug  = true // If true, some debugging info is printed to the javascript console
+	width, height      float64
 	rCall              js.Callback
 	ctx, doc, canvasEl js.Value
+	//debug  = true // If true, some debugging info is printed to the javascript console
 )
 
 func main() {
@@ -53,11 +64,14 @@ func main() {
 	ctx = canvasEl.Call("getContext", "2d")
 
 	// Seed the random generator (only used for colour generation), with a value that generates a "known ok" colour set
-	rand.Seed(7)
+	rand.Seed(3)
 
 	// Add some objects to the world space
 	worldSpace = make(map[string]Object, 1)
 	worldSpace["ob1"] = importObject(object1, 3.0, 3.0, 0.0)
+	worldSpace["ob1 copy"] = importObject(object1, -3.0, 3.0, 0.0)
+	worldSpace["ob2"] = importObject(object2, 3.0, -3.0, 1.0)
+	worldSpace["ob3"] = importObject(object3, -3.0, 0.0, -1.0)
 
 	//// Simple keyboard handler for catching the arrow, WASD, and numpad keys
 	//// Key value info can be found here: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
@@ -266,9 +280,8 @@ func renderFrame(args []js.Value) {
 				// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setLineDash
 				//ctx.Set("setLineDash", "{5, 15}")
 				//ctx.Set("setLineDash", "5, 15")
-				//ctx.Set("setLineDash", "(5, 15)")
 				//ctx.Set("setLineDash", "[5, 15]")
-				//ctx.Call("setLineDash", 5, 15)
+				//ctx.Set("setLineDash", "\"[5, 15]\"")
 				ctx.Call("moveTo", px, py)
 				ctx.Call("lineTo", tx, ty)
 				ctx.Call("stroke")
@@ -280,7 +293,7 @@ func renderFrame(args []js.Value) {
 
 				// Draw lighter coloured legend text
 				ctx.Set("font", "12px sans-serif")
-				ctx.Call("fillText", fmt.Sprintf("(%0.1f, %0.1f, %0.1f)", l.X, l.Y, l.Z), graphWidth+85, l.Num*25)
+				ctx.Call("fillText", fmt.Sprintf("(%0.1f, %0.1f, %0.1f)", l.X, l.Y, l.Z), graphWidth+100, l.Num*25)
 				pointNum++
 			}
 		}
@@ -313,4 +326,132 @@ func importObject(ob Object, x float64, y float64, z float64) (translatedObject 
 		pointCounter++
 	}
 	return translatedObject
+}
+
+// Multiplies one matrix by another
+func matrixMult(opMatrix matrix, m matrix) (resultMatrix matrix) {
+	top0 := m[0]
+	top1 := m[1]
+	top2 := m[2]
+	top3 := m[3]
+	upperMid0 := m[4]
+	upperMid1 := m[5]
+	upperMid2 := m[6]
+	upperMid3 := m[7]
+	lowerMid0 := m[8]
+	lowerMid1 := m[9]
+	lowerMid2 := m[10]
+	lowerMid3 := m[11]
+	bot0 := m[12]
+	bot1 := m[13]
+	bot2 := m[14]
+	bot3 := m[15]
+
+	resultMatrix = matrix{
+		(opMatrix[0] * top0) + (opMatrix[1] * upperMid0) + (opMatrix[2] * lowerMid0) + (opMatrix[3] * bot0), // 1st col, top
+		(opMatrix[0] * top1) + (opMatrix[1] * upperMid1) + (opMatrix[2] * lowerMid1) + (opMatrix[3] * bot1), // 2nd col, top
+		(opMatrix[0] * top2) + (opMatrix[1] * upperMid2) + (opMatrix[2] * lowerMid2) + (opMatrix[3] * bot2), // 3rd col, top
+		(opMatrix[0] * top3) + (opMatrix[1] * upperMid3) + (opMatrix[2] * lowerMid3) + (opMatrix[3] * bot3), // 4th col, top
+
+		(opMatrix[4] * top0) + (opMatrix[5] * upperMid0) + (opMatrix[6] * lowerMid0) + (opMatrix[7] * bot0), // 1st col, upper middle
+		(opMatrix[4] * top1) + (opMatrix[5] * upperMid1) + (opMatrix[6] * lowerMid1) + (opMatrix[7] * bot1), // 2nd col, upper middle
+		(opMatrix[4] * top2) + (opMatrix[5] * upperMid2) + (opMatrix[6] * lowerMid2) + (opMatrix[7] * bot2), // 3rd col, upper middle
+		(opMatrix[4] * top3) + (opMatrix[5] * upperMid3) + (opMatrix[6] * lowerMid3) + (opMatrix[7] * bot3), // 4th col, upper middle
+
+		(opMatrix[8] * top0) + (opMatrix[9] * upperMid0) + (opMatrix[10] * lowerMid0) + (opMatrix[11] * bot0), // 1st col, lower middle
+		(opMatrix[8] * top1) + (opMatrix[9] * upperMid1) + (opMatrix[10] * lowerMid1) + (opMatrix[11] * bot1), // 2nd col, lower middle
+		(opMatrix[8] * top2) + (opMatrix[9] * upperMid2) + (opMatrix[10] * lowerMid2) + (opMatrix[11] * bot2), // 3rd col, lower middle
+		(opMatrix[8] * top3) + (opMatrix[9] * upperMid3) + (opMatrix[10] * lowerMid3) + (opMatrix[11] * bot3), // 4th col, lower middle
+
+		(opMatrix[12] * top0) + (opMatrix[13] * upperMid0) + (opMatrix[14] * lowerMid0) + (opMatrix[15] * bot0), // 1st col, bottom
+		(opMatrix[12] * top1) + (opMatrix[13] * upperMid1) + (opMatrix[14] * lowerMid1) + (opMatrix[15] * bot1), // 2nd col, bottom
+		(opMatrix[12] * top2) + (opMatrix[13] * upperMid2) + (opMatrix[14] * lowerMid2) + (opMatrix[15] * bot2), // 3rd col, bottom
+		(opMatrix[12] * top3) + (opMatrix[13] * upperMid3) + (opMatrix[14] * lowerMid3) + (opMatrix[15] * bot3), // 4th col, bottom
+	}
+	return resultMatrix
+}
+
+// Rotates a transformation matrix around the X axis by the given degrees
+func rotateAroundX(m matrix, degrees float64) matrix {
+	rad := (math.Pi / 180) * degrees // The Go math functions use radians, so we convert degrees to radians
+	rotateXMatrix := matrix{
+		1, 0, 0, 0,
+		math.Cos(rad), 0, -math.Sin(rad), 0,
+		math.Sin(rad), 0, math.Cos(rad), 0,
+		0, 0, 0, 1,
+	}
+	return matrixMult(rotateXMatrix, m)
+}
+
+// Rotates a transformation matrix around the Y axis by the given degrees
+func rotateAroundY(m matrix, degrees float64) matrix {
+	rad := (math.Pi / 180) * degrees // The Go math functions use radians, so we convert degrees to radians
+	rotateYMatrix := matrix{
+		math.Cos(rad), 0, math.Sin(rad), 0,
+		0, 1, 0, 0,
+		-math.Sin(rad), 0, math.Cos(rad), 0,
+		0, 0, 0, 1,
+	}
+	return matrixMult(rotateYMatrix, m)
+}
+
+// Rotates a transformation matrix around the Z axis by the given degrees
+func rotateAroundZ(m matrix, degrees float64) matrix {
+	rad := (math.Pi / 180) * degrees // The Go math functions use radians, so we convert degrees to radians
+	rotateZMatrix := matrix{
+		math.Cos(rad), -math.Sin(rad), 0, 0,
+		math.Sin(rad), math.Cos(rad), 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1,
+	}
+	return matrixMult(rotateZMatrix, m)
+}
+
+// Scales a transformation matrix by the given X, Y, and Z values
+func scale(m matrix, x float64, y float64, z float64) matrix {
+	scaleMatrix := matrix{
+		x, 0, 0, 0,
+		0, y, 0, 0,
+		0, 0, z, 0,
+		0, 0, 0, 1,
+	}
+	return matrixMult(scaleMatrix, m)
+}
+
+// Translates (moves) a transformation matrix by the given X, Y and Z values
+func translate(m matrix, translateX float64, translateY float64, translateZ float64) matrix {
+	translateMatrix := matrix{
+		1, 0, 0, translateX,
+		0, 1, 0, translateY,
+		0, 0, 1, translateZ,
+		0, 0, 0, 1,
+	}
+	return matrixMult(translateMatrix, m)
+}
+
+// Transform the XYZ co-ordinates using the values from the transformation matrix
+func transform(m matrix, p Point) (t Point) {
+	top0 := m[0]
+	top1 := m[1]
+	top2 := m[2]
+	top3 := m[3]
+	upperMid0 := m[4]
+	upperMid1 := m[5]
+	upperMid2 := m[6]
+	upperMid3 := m[7]
+	lowerMid0 := m[8]
+	lowerMid1 := m[9]
+	lowerMid2 := m[10]
+	lowerMid3 := m[11]
+	//bot0 := m[12] // The fourth row values can be ignored for 3D matrices
+	//bot1 := m[13]
+	//bot2 := m[14]
+	//bot3 := m[15]
+
+	t.Colour = p.Colour
+	t.Num = p.Num
+	t.X = (top0 * p.X) + (top1 * p.Y) + (top2 * p.Z) + top3
+	t.Y = (upperMid0 * p.X) + (upperMid1 * p.Y) + (upperMid2 * p.Z) + upperMid3
+	t.Z = (lowerMid0 * p.X) + (lowerMid1 * p.Y) + (lowerMid2 * p.Z) + lowerMid3
+	return
 }
